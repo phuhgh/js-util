746e8df9908a38a189761989c985bef1ef5c1df0
Jack Bromley <phuhgh@gmail.com>
2017-10-17 22:31:13 +0100

    .

---
+++ index.ts
@@ -0,0 +1,18 @@
+﻿export interface IDictionary<T>
+{
+    [index: string]: T;
+}
+
+export function undictionaryify<T>(d: IDictionary<T>): T[]
+{
+    const keys = Object.keys(d);
+    const l = keys.length;
+    const a = new Array<T>(keys.length);
+    
+    for (let i = 0; i < l; ++i)
+    {
+        a[i] = d[keys[i]];
+    }
+
+    return a;
+}
