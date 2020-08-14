41ab6f7b3388004cc30801647385fe3429d4c7f0
Jack Bromley <phuhgh@gmail.com>
2020-08-14 12:47:21 +0100

    isArray: improve typing

--- src/array/impl/array-is-array.ts
+++ src/array/impl/array-is-array.ts
@@ -1,4 +1,4 @@
-export const arrayIsArray = (obj: unknown) =>
+export const arrayIsArray = (obj: unknown): obj is ArrayLike<unknown> =>
 {
     return obj instanceof Array || ArrayBuffer.isView(obj);
 };
