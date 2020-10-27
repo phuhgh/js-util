ba2d44d560b0c83e80aef5be9269ac692b497d1f
Jack Bromley <phuhgh@gmail.com>
2020-10-27 12:28:22 +0000

    expose fp reject false

--- src/fp/_fp.ts
+++ src/fp/_fp.ts
@@ -2,6 +2,7 @@ import { fpIdentity } from "./impl/fp-identity";
 import { fpNormalizeToNull } from "./impl/fp-normalize-to-null";
 import { fpNormalizeToUndefined } from "./impl/fp-normalize-to-undefined";
 import { fpOnce } from "./impl/fp-once";
+import { fpRejectFalse } from "./impl/fp-reject-false";
 import { fpRejectNull } from "./impl/fp-reject-null";
 
 // tslint:disable-next-line:class-name
@@ -11,6 +12,7 @@ export class _Fp
     public static readonly normalizeToNull = fpNormalizeToNull;
     public static readonly normalizeToUndefined = fpNormalizeToUndefined;
     public static readonly once = fpOnce;
+    public static readonly rejectFalse = fpRejectFalse;
     public static readonly rejectNull = fpRejectNull;
 
     private constructor()
