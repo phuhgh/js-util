676522c716c93da2847d0888344b187c1b1a88ee
Jack Bromley <phuhgh@gmail.com>
2021-04-20 11:10:48 +0100

    add missing test

---
+++ src/fp/impl/fp-once.spec.ts
@@ -0,0 +1,26 @@
+import { fpOnce } from "./fp-once";
+import createSpy = jasmine.createSpy;
+
+describe("=> fpOnce", () =>
+{
+    it("| calls the function once with the expected arguments", () =>
+    {
+        const spy = createSpy<(a: number, b: number, c: number) => number>();
+        const oncedSpy = fpOnce(spy);
+
+        oncedSpy(1, 2, 3);
+        // @ts-expect-error - should be a compile error, signature doesn't match
+        oncedSpy(1, 2, 3, 4);
+
+        expect(spy).toHaveBeenCalledOnceWith(1, 2, 3);
+    });
+
+    it("| returns the callback's original return each time", () =>
+    {
+        let ret = 0;
+        const oncedSpy = fpOnce(() => ++ret);
+
+        expect(oncedSpy()).toBe(1);
+        expect(oncedSpy()).toBe(1);
+    });
+});
