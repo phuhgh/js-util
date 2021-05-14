5fe81a976c71122702d8042bf531aa09a3a7ded9
Jack Bromley <phuhgh@gmail.com>
2021-05-14 17:47:33 +0100

    empty array: remove debug check on module creation

--- src/array/impl/array-empty-array.ts
+++ src/array/impl/array-empty-array.ts
@@ -1,12 +1,5 @@
-import { _Debug } from "../../debug/_debug";
-
 /**
  * @public
  * An empty readonly array, useful to avoid GC pressure.
  */
 export const arrayEmptyArray: readonly [] = [];
-
-DEBUG_MODE && _Debug.runBlock(() =>
-{
-    Object.freeze(arrayEmptyArray);
-});
