4d540a7259afc347cda5b103201f90508a9bad25
Jack Bromley <phuhgh@gmail.com>
2021-10-22 16:10:51 +0100

    incrementing id factory: start from 1 by default (so subtracting MAX_SAFE_INTEGER > MIN_SAFE_INTEGER)

--- src/identifiers/incrementing-identifier-factory.ts
+++ src/identifiers/incrementing-identifier-factory.ts
@@ -5,12 +5,12 @@ import { IIdentifierFactory } from "./i-identifier-factory";
  * Provides an incrementing integer identifier.
  *
  * @remarks
- * By default the first ID will be 0, this may be configured at construction.
+ * By default the first ID will be 1, this may be configured at construction.
  */
 export class IncrementingIdentifierFactory implements IIdentifierFactory
 {
     public constructor(
-        private id = 0,
+        private id = 1,
     )
     {
     }
