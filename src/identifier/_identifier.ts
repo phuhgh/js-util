68303079a6f49529ec5aaa25f5417a18b164248e
Jack Bromley <phuhgh@gmail.com>
2021-10-26 17:50:36 +0100

    add _Identifier

---
+++ src/identifier/_identifier.ts
@@ -0,0 +1,23 @@
+import { IncrementingIdentifierFactory } from "./impl/incrementing-identifier-factory";
+
+/**
+ * @public
+ * Utilities for generating identifiers.
+ */
+export class _Identifier
+{
+    /**
+     * @public
+     * Returns an integer that increases by 1 every call, starting from 1.
+     */
+    public static getNextIncrementingId(): number
+    {
+        return _Identifier.globalUidFactory.getNextId();
+    }
+
+    private constructor()
+    {
+    }
+
+    private static globalUidFactory = new IncrementingIdentifierFactory();
+}
