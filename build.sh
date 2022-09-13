3831b07500444c4a7edd706cc1862a18e62db1e4
Jack Bromley <phuhgh@gmail.com>
2022-09-13 20:51:51 +0100

    windows support with msys

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
-env node scripts/extract-cpp-names.mjs bin/esm/cpp.module.js > cpp/exported-names.txt || exit
+env node scripts/extract-cpp-names.mjs bin/esm/cpp.module.js >cpp/exported-names.txt || exit
 cd cpp || exit
 rm -rf build
 mkdir build
 cd build || exit
-emcmake cmake -G Ninja ../.. -DCMAKE_BUILD_TYPE=Debug -DBUILD_JSUTIL_TEST=1 || exit
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+  emcmake.bat cmake -G Ninja ../.. -DCMAKE_BUILD_TYPE=Debug -DBUILD_JSUTIL_TEST=1 || exit
+else
+  emcmake cmake -G Ninja ../.. -DCMAKE_BUILD_TYPE=Debug -DBUILD_JSUTIL_TEST=1 || exit
+fi
+
 cmake --build . || exit
 mv cpp/asan-test-module.js cpp/asan-test-module.cjs || exit
 mv cpp/safe-heap-test-module.js cpp/safe-heap-test-module.cjs || exit
