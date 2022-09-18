89be7212c1a245881e0b4494c922d3369ac9ca04
Jack Bromley <phuhgh@gmail.com>
2022-09-18 14:51:07 +0100

    correct build scripts

--- scripts/build-cpp.sh
+++ scripts/build-cpp.sh
@@ -1,12 +1,16 @@
+#!/bin/sh
+
+set -e
+
 SCRIPTS_DIR="${BASH_SOURCE%/*}"
 
-if [[ ! -d "$DIR" ]]; then
+if [ ! -d "$DIR" ]; then
   SCRIPTS_DIR="$PWD"
 fi
 
 print_instructions() {
   echo "================================="
-  echo "usage: build-cpp.sh {path/to/cpp-bindings.js} {path/to/cpp/cmake/dir} {mjs|cjs} {debug|release} {cmake_build_flags}"
+  echo "usage: build-cpp.sh {path/to/cpp-bindings.js} {path/to/cpp/cmake/dir} {mjs|cjs} {debug|release} {cmake_build_flags} {clean|}"
   echo "builds the c++ given the js binding descriptor and cmake project"
   echo "================================="
   exit 1
@@ -17,24 +21,29 @@ CMAKE_PROJ_DIR="$2"
 MODULE_LOADER="$3"
 BUILD_MODE="$4"
 BUILD_FLAGS="$5"
+BUILD_CLEAN="$6"
 
-if [[ ! "$JS_BINDINGS_FILE" || ! "$CMAKE_PROJ_DIR" || ("$BUILD_MODE" != "debug" && "$BUILD_MODE" != "release") ]]; then
+if [ -z "$JS_BINDINGS_FILE" ] || [ -z "$CMAKE_PROJ_DIR" ]; then
   print_instructions
 fi
 
-if [[ ! -f "$JS_BINDINGS_FILE" ]]; then
+if [ "$BUILD_MODE" != "debug" ] && [ "$BUILD_MODE" != "release" ]; then
+  print_instructions
+fi
+
+if [ ! -f "$JS_BINDINGS_FILE" ]; then
   echo "ERROR: couldn't find js bindings file ${JS_BINDINGS_FILE}"
   print_instructions
 fi
 
-if [[ ! -d "$CMAKE_PROJ_DIR" ]]; then
+if [ ! -d "$CMAKE_PROJ_DIR" ]; then
   echo "ERROR: directory ${1} does not exist"
   print_instructions
 fi
 
-if [[ "$MODULE_LOADER" == "cjs" ]]; then
+if [ "$MODULE_LOADER" = "cjs" ]; then
   node "$SCRIPTS_DIR/scripts/extract-cpp-names.cjs" "$JS_BINDINGS_FILE" >"$CMAKE_PROJ_DIR/exported-names.txt" || exit
-elif [[ "$MODULE_LOADER" == "mjs" ]]; then
+elif [ "$MODULE_LOADER" = "mjs" ]; then
   node "$SCRIPTS_DIR/scripts/extract-cpp-names.mjs" "$JS_BINDINGS_FILE" >"$CMAKE_PROJ_DIR/exported-names.txt" || exit
 else
   print_instructions
@@ -42,8 +51,7 @@ fi
 
 cd "$CMAKE_PROJ_DIR" || exit
 
-# todo jack
-if [ "$1" = "clean" ]; then
+if [ "$BUILD_CLEAN" = "clean" ]; then
   echo "Deleting build dir"
   rm -rf build
 fi
@@ -51,10 +59,12 @@ fi
 mkdir -p build || exit
 cd build || exit
 
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+# these are defined if we're running on windows, we otherwise don't care...
+# shellcheck disable=SC2039
+if [ "$OSTYPE" = "msys" ] || [ "$OSTYPE" = "win32" ]; then
   emcmake.bat cmake -G Ninja .. -DCMAKE_BUILD_TYPE="$BUILD_MODE" "$BUILD_FLAGS" || exit
 else
-  emcmake cmake -G Ninja .. -DCMAKE_BUILD_TYPE="$BUILD_MODE"  "$BUILD_FLAGS" || exit
+  emcmake cmake -G Ninja .. -DCMAKE_BUILD_TYPE="$BUILD_MODE" "$BUILD_FLAGS" || exit
 fi
 
 cmake --build . || exit
--- scripts/copy-test-modules.sh
+++ scripts/copy-test-modules.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+set -e
+
 # rename for module loader
 mv cpp/build/asan-test-module.js cpp/build/asan-test-module.cjs || exit
 mv cpp/build/safe-heap-test-module.js cpp/build/safe-heap-test-module.cjs || exit
